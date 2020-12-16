4104abbfc0c4cd39b3eebfe4b73072d18fdd4243
A N <foto.arn@gmail.com>
2020-12-16 02:02:00 +0100

    December Slack

--- src/Components/SidebarOption.js
+++ src/Components/SidebarOption.js
@@ -5,7 +5,14 @@ function SidebarOption({ Icon, title }) {
 	return (
 		<SidebarOptionContainer>
 			{Icon && <Icon fontSize="small" style={{ padding: 10 }} />}
-			{Icon ? <h3>{title}</h3> : <SidebarOptionChannel></SidebarOptionChannel>}
+			{Icon ? (
+				<h3>{title}</h3>
+			) : (
+				<SidebarOptionChannel>
+					<span>#</span>
+					{title}
+				</SidebarOptionChannel>
+			)}
 		</SidebarOptionContainer>
 	);
 }
