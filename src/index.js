a3a99f3d71f72b2a0a5e14034901b2de855dcc47
Anthony N <foto.arn@gmail.com>
2021-02-22 17:56:07 -0800

    + initial project setup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./Css/index.css";
 import App from "./Js/App";
-import reportWebVitals from "./Js/reportWebVitals";
 
 ReactDOM.render(
 	<React.StrictMode>
