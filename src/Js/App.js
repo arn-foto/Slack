eec8f69b76cf6a2f5406babfad855bb012197af5
Anthony N <foto.arn@gmail.com>
2021-02-22 21:58:58 -0800

    + React-Router set-up

--- src/Js/App.js
+++ src/Js/App.js
@@ -6,6 +6,37 @@ function App() {
 	return (
 		<div className="App">
 			<h1>Slack (evil-clone)</h1>
+			<Router>
+				<div>
+					<nav>
+						<ul>
+							<li>
+								<Link to="/">Home</Link>
+							</li>
+							<li>
+								<Link to="/about">About</Link>
+							</li>
+							<li>
+								<Link to="/users">Users</Link>
+							</li>
+						</ul>
+					</nav>
+
+					{/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+					<Switch>
+						<Route path="/about">
+							<About />
+						</Route>
+						<Route path="/users">
+							<Users />
+						</Route>
+						<Route path="/">
+							<Home />
+						</Route>
+					</Switch>
+				</div>
+			</Router>
 		</div>
 	);
 }
