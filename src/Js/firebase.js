4e9a999bcf2948c311e6e23d35fe1fb257dc08ab
Anthony N <foto.arn@gmail.com>
2021-02-22 21:18:49 -0800

    + Firebase setup

---
+++ src/Js/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyD-UqoEst9kIwYt3q8RtVsrhsN8QMjZvqU",
+	authDomain: "slack-clone-14ee8.firebaseapp.com",
+	projectId: "slack-clone-14ee8",
+	storageBucket: "slack-clone-14ee8.appspot.com",
+	messagingSenderId: "1017655478059",
+	appId: "1:1017655478059:web:3ed7aa191c6cf070d5e76e",
+};
