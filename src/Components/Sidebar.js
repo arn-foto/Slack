f2a8d9bfa3e9c4d33cf6ef6f02ef70be958c816a
A N <foto.arn@gmail.com>
2021-03-07 17:26:46 -0800

    + Add 'Sidebar' styled-component

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import CreateIcon from "@material-ui/icons/Create";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 
 function Sidebar() {
@@ -13,6 +14,7 @@ function Sidebar() {
 						Anthony Navarro
 					</h3>
 				</SidebarInfo>
+				<CreateIcon />
 			</SidebarHeader>
 		</SidebarContainer>
 	);
@@ -29,6 +31,18 @@ const SidebarContainer = styled.div`
 	margin-top: 60px;
 `;
 
-const SidebarHeader = styled.div``;
+const SidebarHeader = styled.div`
+	display: flex;
+	border-bottom: 1px solid #49274b;
+	padding: 13px;
+
+	> .MuiSvgIcon-root {
+		padding: 8px;
+		color: #49274b;
+		font-size: 18px;
+		background-color: white;
+		border-radius: 999px;
+	}
+`;
 
 const SidebarInfo = styled.div``;
