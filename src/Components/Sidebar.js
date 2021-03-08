2a87a9fb15fe80e3337715abea2285c736f4a918
A N <foto.arn@gmail.com>
2021-03-08 14:47:26 -0800

    + Create reusable sidebar component

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -2,6 +2,15 @@ import React from "react";
 import styled from "styled-components";
 import CreateIcon from "@material-ui/icons/Create";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+import InsertCommentIcon from "@material-ui/icons/InsertComment";
+import InboxIcon from "@material-ui/icons/Inbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import AppsIcon from "@material-ui/icons/Apps";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import SidebarOption from "../Components/SidebarOption";
 
 function Sidebar() {
 	return (
