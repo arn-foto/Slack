e55b8bc0f8d495a1193833b958b78eba92475d80
A N <foto.arn@gmail.com>
2021-03-07 16:39:05 -0800

    + Add 'Sidebar' component

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,7 +1,10 @@
 import React from "react";
+import styled from "styled-components";
 
 function Sidebar() {
-	return <div></div>;
+	return <SidebarContainer></SidebarContainer>;
 }
 
 export default Sidebar;
+
+const SidebarContainer = styled.div``;
