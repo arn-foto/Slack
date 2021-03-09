e007ab1a2acb14d68d5979647d04eb50d5b0a68a
A N <foto.arn@gmail.com>
2021-03-08 18:56:02 -0800

    + Create ChatContainer component

---
+++ src/Components/Chat.js
@@ -0,0 +1,10 @@
+import { styled } from "@material-ui/core";
+import React from "react";
+
+function Chat() {
+	return <ChatContainer></ChatContainer>;
+}
+
+export default Chat;
+
+const ChatContainer = styled.div``;
